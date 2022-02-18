af85656c11869385aef989a1c330dbe35ceeff5f
you@e <you@e>
2022-02-18 21:35:26 +0800

    提交

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ function login()
 	var username=document.getElementById("username").value;
 	var password=document.getElementById("password").value;
 	if(password==pass){
-		window.open("index.html",'_self');
+		window.open("view0.html",'_self');
 	}else{
 		alert("密码错误");
 	}
